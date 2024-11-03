24490f44ba5d76af6b34ab13ca81c7e169cd3b7e
Robert Kurcina <kitrok@gmail.com>
2024-11-02 19:05:20 -0700

    added an App.jsx title.

--- src/App.jsx
+++ src/App.jsx
@@ -19,25 +19,28 @@ function App() {
     setOutput(".");
   }
 
-  function handleEdit(e){
+  function handleEdit(e) {
     const value = e.target.value || "";
     setText(value);
   }
 
   return (
-    <div id="application">
-      <h1>Given</h1>
-      <p>Given a list of trait, target, level, created a nicely formatted string for presentation as a <em>MEST Tactics</em> traits paragraph.</p>
-      <textarea id="inputs" value={text} onChange={handleEdit}></textarea>
-      <div id="controls">
-        <button onClick={handleProcess}>PROCESS</button>
-        <button onClick={handleReset}>RESET</button>
-      </div>
+    <>
+      <div id="title">MEST Tactics :: Excel Parser</div>
+      <div id="application">
+        <h1>Given</h1>
+        <p>Given a list of trait, target, level, created a nicely formatted string for presentation as a <em>MEST Tactics</em> traits paragraph.</p>
+        <textarea id="inputs" value={text} onChange={handleEdit}></textarea>
+        <div id="controls">
+          <button onClick={handleProcess}>PROCESS</button>
+          <button onClick={handleReset}>RESET</button>
+        </div>
 
-      <h1>Output</h1>
-      <p>Click the <strong>PROCESS</strong> button to see the formatted result of the input text from above.</p>
-      <code>{output}</code>
-    </div>
+        <h1>Output</h1>
+        <p>Click the <strong>PROCESS</strong> button to see the formatted result of the input text from above.</p>
+        <code>{output}</code>
+      </div>
+    </>
   )
 }
 
