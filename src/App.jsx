4b117ceaff23d87e43ea393420632aeea10287e2
Robert Kurcina <kitrok@gmail.com>
2024-11-02 18:04:49 -0700

    updated inputStr with more expressive examples.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import traitAndLevels from './parser';
 
-const inputStr = 'Exert X > List, Blast:Major, 1, Exert X > List, Augment, 1, Slippery X, ., 3, [Arc X], ., 4, ., ., 0, ., ., 0, [Addicted X > Type], ., 0, ., ., 0, ., ., 0, ., ., 0, ., ., 0, Aura X > List, Acid-1, 0';
+const inputStr = '[Exert X > List], Blast:Major, 1, [Exert X > List], Augment, 1, Slippery X, ., 3, [Arc X], ., 4, ., ., 0, ., ., 0, [Addicted X > Type], Narcotics, 0, ., ., 0, ., ., 0, ., ., 0, ., ., 0, Aura X > List, Acid-1, 0';
 
 function App() {
   let resultStr = traitAndLevels(inputStr);
