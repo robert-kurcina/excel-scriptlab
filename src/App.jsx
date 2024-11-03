34b4f5664f1d8308ab7cf97c68966b1f39b02b00
Robert Kurcina <kitrok@gmail.com>
2024-11-02 18:39:24 -0700

    some formating for CSS added.

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,34 @@ import './App.css'
 import traitAndLevels from './parser';
 import inputStr from './sample.input';
 
-function handleClick(givenStr){
-  let resultStr = traitAndLevels(givenStr);
-}
-
 function App() {
+  const [text, setText] = useState(inputStr);
+  const [output, setOutput] = useState(".");
+
+  function handleProcess() {
+    const textInput = document.getElementById("inputs").textContent;
+    let resultStr = traitAndLevels(textInput);
+    setOutput(resultStr);
+  }
+
+  function handleReset() {
+    setText(inputStr);
+    setOutput(".");
+  }
+
   return (
     <div id="application">
       <h1>Given</h1>
-      <textarea>{inputStr}</textarea>
-      <button onClick={handleClick(inputStr)}>PROCESS</button>
+      <p>Given a list of trait, target, level, created a nicely formatted string for presentation as a MEST Tactics traits paragraph.</p>
+      <textarea id="inputs" value={text} onChange={setText}></textarea>
+      <div id="controls">
+        <button onClick={handleProcess}>PROCESS</button>
+        <button onClick={handleReset}>RESET</button>
+      </div>
 
       <h1>Output</h1>
-      <code>.</code>
+      <p>Click the PROCESS button to see the formatted result of the input text from above.</p>
+      <code>{output}</code>
     </div>
   )
 }
